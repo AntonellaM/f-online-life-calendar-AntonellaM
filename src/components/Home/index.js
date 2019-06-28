ec05eebef003ccae827fc92e51664b242f2970c5
Antonella Michelli <antonella.michelli@upplication.com>
2019-06-28 11:26:09 +0200

    Add home styles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import Date from '../Date';
+import './styles.scss';
 
 const Home = (props) => {
-  console.log('home moodregister', props.moodRegister)
   return (
     <React.Fragment>
-      <header><Link to="/add-date"><button>Añadir</button></Link></header>
-      <main>
+      <header className="home__header"><Link to="/add-date"><button className="home__header-button--add"><FontAwesomeIcon icon={faPlus} /></button></Link></header>
+      <main className="home__main">
         {props.moodRegister
         ?
-        <ul>{props.moodRegister.map(dailyRegister => <li><Date dateRegister={dailyRegister}/></li>)}</ul>
+        <ul className="mood-list">{props.moodRegister.map(dailyRegister => <li className="mood-item"><Date dateRegister={dailyRegister}/></li>)}</ul>
         :
         <p>No hay datos</p>
         }
