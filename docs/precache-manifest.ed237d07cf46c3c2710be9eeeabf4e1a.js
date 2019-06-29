058710de00f6b560cc646ae1f2527bcb7037d07d
antonellam <antonella.michelli@gmail.com>
2019-06-29 16:59:23 +0200

    Add docs folder

---
+++ docs/precache-manifest.ed237d07cf46c3c2710be9eeeabf4e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d3b826d51db48deba3f1b643a086e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "92d535fc3672acf0bbf4",
+    "url": "./static/css/main.9e609a1d.chunk.css"
+  },
+  {
+    "revision": "54991de399e2760290c5",
+    "url": "./static/js/2.f0a39f3c.chunk.js"
+  },
+  {
+    "revision": "92d535fc3672acf0bbf4",
+    "url": "./static/js/main.fac69731.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "982879b96c6be9cc5faa919d81bff57b",
+    "url": "./static/media/background.982879b9.png"
+  }
+]);
